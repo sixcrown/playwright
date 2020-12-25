bb86a2391d8ea2eecdcb33e90c787d7bdbbd0310
sixcrown <kartoszbawalkiewicz@gmail.com>
2020-12-25 18:00:00 +0100

    server

--- src/index.js
+++ src/index.js
@@ -49,26 +49,48 @@ app.get("/my/:syf", async (req, res) => {
 async function sendSpecialCharacter(page, selector, key) {
   const elementHandle = await page.$(selector);
   await elementHandle.press(key);
+  
 }
+  const width = req.query.width ? parseInt(req.query.width, 10) : 1920
+  const height = req.query.height ? parseInt(req.query.height, 10) : 1080
   const browser = await chromium.launch({
     chromiumSandbox: false
   });
   const context = await browser.newContext();
-  const page = await context.newPage();
+  const page = await context.newPage({
+    viewport: {
+      width,
+      height
+    }
+  });
   await page.goto("https://www.familysearch.org/en/");
+  console.log("1 step");
   await page.click("#signInLink");
+  console.log("2 step");
   await page.click("#userName");
+  console.log("3 step");
   await page.type("#userName", 'sixcrown');
+  console.log("4 step");
   await sendSpecialCharacter(page, "#userName", 'Tab');
+  console.log("5 step");
   await page.type("#password", 'Troj@n!3');
+  console.log("6 step");
   await page.click("#login");
+  console.log("7 step");
   await page.click("[aria-controls='search']");
+  console.log("8 step");
   await page.click("#search > li:nth-child(1) > a");
+  console.log("9 step");
   await page.click("#surname");
+  console.log("10 step");
   await page.click("#surname");
+  console.log("11 step");
   await page.type("#surname", 'kawalkiewicz');
+  console.log("12 step");
   await sendSpecialCharacter(page, "#surname", 'Enter');
+  console.log("13 step");
   await page.click("[image-url='/ark:/61903/3:1:3QSQ-G9MB-PVK9?personaUrl=%2Fark%3A%2F61903%2F1%3A1%3AKW16-NPN'] .image-button");
+  console.log("14 step");
   try{
       const data = await page.screenshot({
       type: "png"
